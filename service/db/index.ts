820f705867c5cb1e4b99696282bf0a093e41e27d
Guillaume VARA <guillaume.vara@gmail.com>
2025-05-15 14:44:21 +0200

    Add specs + extra migrations (#4)
    * specs
    
    * spec update
    
    * bump 1.4.2 - Add extra-migrations capabilities

--- service/db/index.ts
+++ service/db/index.ts
@@ -8,22 +8,42 @@ import { title } from "#/helpers/static";
 import logr from "#/server/helpers/loggers";
 
 //
-const readyingDB = (dbFilePath: string | null) => {
-  //
-  if (!dbFilePath) {
-    const message =
-      "Call to defineDbCharacteristics is required before using database.";
-    logr.error(message);
-    throw new Error(message);
-  }
+//
+//
 
+/**
+ * @dev Adapt to your needs. Theses will be accessible to your extra migrations
+ * @example
+ * type ExtraMigrationsPayload = {
+ *    defaultAdminUser: string;
+ * }
+ */
+type ExtraMigrationsPayload = null;
+
+//
+//
+//
+
+/**
+ * @dev may adapt the logic to your needs, depending on DB technology (sqlite, postgres...)
+ */
+type DatabasePrewarmConfig = {
+  dbFilePath: string;
+  extraMigrationsPayload?: ExtraMigrationsPayload;
+};
+
+/**
+ * Ensures migrations and maintenance tasks are due
+ * @dev may adapt the logic to your needs, depending on DB technology (sqlite, postgres...)
+ * */
+export const readyingDB = async (options: DatabasePrewarmConfig) => {
   //
   logr.log("Initiating DB...");
-  mkdirSync(dbFilePath, { recursive: true });
+  mkdirSync(options.dbFilePath, { recursive: true });
 
   //
   const appDbName = title.toLowerCase();
-  const url = `file:${dbFilePath}/${appDbName}.db`;
+  const url = `file:${options.dbFilePath}/${appDbName}.db`;
 
   //
   logr.log("Loading SQLite DB from", url);
@@ -34,28 +54,113 @@ const readyingDB = (dbFilePath: string | null) => {
 
   //
   logr.log("Starting DB Schema Migration...");
-  migrate(db, { migrationsFolder: "./drizzle" });
+  await migrate(db, { migrationsFolder: "./drizzle" });
   logr.log("DB Schema Migration Done");
 
   //
+  await maybeProcessExtraMigrations(db, options.extraMigrationsPayload);
+
+  //
   logr.log("Database Ready !");
-  return db;
+
+  //
+  _db = db;
 };
 
 //
-let db: ReturnType<typeof readyingDB> | null = null;
-
-//
 //
 //
 
 //
-let _dbFilePath: string | null = null;
-export const defineDbCharacteristics = (options: { dbFilePath: string }) => {
-  _dbFilePath = options.dbFilePath;
+type DBType = ReturnType<typeof drizzle>;
+
+//
+type ExtraMigrationsConfig = Array<{
+  /** description of the job */
+  description: string;
+
+  /** What to do for this job */
+  job: (
+    /** Transaction handler, use to apply migrations (similar to "db", but automatically rollback on failure) */
+    tx: Parameters<Parameters<DBType["transaction"]>[0]>[0],
+
+    /** Used to customize behavior of your job; put there names, credentials, group names... */
+    payload?: ExtraMigrationsPayload,
+  ) => Promise<void>;
+}>;
+
+/**
+ * @dev
+ * Define here any extra migrations to be ran after Drizzle migrations.
+ *
+ * These will be executed sequentially.
+ *
+ * If any of those may fail, will rollback all operations.
+ *
+ * @example
+ * const extraMigrations: ExtraMigrationsConfig = [
+ *  {
+ *    description: "May create default admin user",
+ *    job: async (tx, payload) => {
+ *      //
+ *      const { username } = payload;
+ *
+ *      // Put here any step (filling tables...)
+ *      await tx.insert(mytable).values({ username, ...})
+ *    },
+ *  },
+ *  // maybe other jobs...
+ * ]
+ *
+ *
+ *
+ */
+const extraMigrations: ExtraMigrationsConfig = [];
+
+//
+const maybeProcessExtraMigrations = async (
+  db: DBType,
+  payload?: ExtraMigrationsPayload,
+) => {
+  //
+  if (extraMigrations.length == 0) return;
+
+  await db.transaction(async (tx) => {
+    //
+    for (const extraMigration of extraMigrations) {
+      logr.log(
+        `Processing extra migration: "${extraMigration.description}"...`,
+      );
+
+      //
+      await extraMigration.job(tx, payload).catch((e) => {
+        logr.log(`Extra migration "${extraMigration.description}" failed !`);
+        throw e;
+      });
+    }
+  });
+
+  //
+  logr.log("Extra migrations OK !");
 };
+
+//
+//
+//
+
+//
+let _db: DBType | null = null;
 
 //
 export const getDb = () => {
-  return db ?? (db = readyingDB(_dbFilePath));
+  // failsafe
+  if (!_db) {
+    const message =
+      'Call to "readyingDB" is required before interacting with the database.';
+    logr.error(message);
+    throw new Error(message);
+  }
+
+  //
+  return _db;
 };
